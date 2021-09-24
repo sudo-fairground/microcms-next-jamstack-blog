5f091aa7253438b6f81b7eec905cfe6aa0f20f39
Mariko Sudo <sudo@fairground.jp>
2021-09-24 20:13:01 +0900

    jQueryとscript.jsを読み込む

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <script defer src="//ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script defer src="/js/script.js"></script>
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
