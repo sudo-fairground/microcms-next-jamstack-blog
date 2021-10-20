39fa90255fae3cb182adcac05cde1ac8e7600620
Mariko Sudo <sudo@fairground.jp>
2021-10-20 21:00:03 +0900

    metaを調整

--- components/CommonMeta.js
+++ components/CommonMeta.js
@@ -1,24 +1,25 @@
 import Head from 'next/head'
 
-const siteinfo = {
+const siteInfo = {
   site_name: "株式会社フェアグラウンド",
   url: "https://www.fairground.jp",
   title: "株式会社フェアグラウンド",
-  description: "株式会社フェアグラウンドはWEBの制作会社です。"
+  description: "株式会社フェアグラウンドはWEBの制作会社です。",
 };
 
-const CommonMeta = ({ title = siteinfo.title, description = siteinfo.description, uri = "/" }) => (
+const CommonMeta = ({ title = siteInfo.title, description = siteInfo.description, uri = "/" }) => (
   <Head>
     <title>{title}</title>
     <meta name="description" content={description} />
     <meta property="og:title" content={title} />
-    <meta property="og:site_name" content={siteinfo.site_name} />
+    <meta property="og:site_name" content={siteInfo.site_name} />
     <meta property="og:type" content="website" />
     <meta property="og:description" content={description} />
     <meta property="og:locale" content="ja_JP" />
-    <meta property="og:url" content={`${siteinfo.url}${uri}`} />
-    <meta property="og:image" content={`${siteinfo.url}/ogp.jpg`} />
+    <meta property="og:url" content={`${siteInfo.url}${uri}`} />
+    <meta property="og:image" content={`${siteInfo.url}/ogp.jpg`} />
     <meta name="twitter:card" content="summary_large_image" />
+    <link rel="canonical" href={`${siteInfo.url}${uri}`} />
   </Head>
 );
 
--- pages/about.js
+++ pages/about.js
@@ -2,9 +2,15 @@ import Link from "next/link";
 import CommonMeta from '../components/CommonMeta.js'
 import Layout from '../components/Layout.js';
 
+const pageMeta = {
+  title: "会社概要 | 株式会社フェアグラウンド",
+  description: "株式会社フェアグラウンドの会社概要です。",
+  uri: "/about/",
+};
+
 const About = () => (
   <>
-    <CommonMeta title="会社概要 | 株式会社フェアグラウンド" description="株式会社フェアグラウンドの会社概要です。" uri="/about/" />
+    <CommonMeta title={pageMeta.title} description={pageMeta.description} uri={pageMeta.uri} />
     <Layout>
       <article className="article">
         <div className="container">
