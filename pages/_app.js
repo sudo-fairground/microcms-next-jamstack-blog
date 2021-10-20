bfc7e8c2efe90b0b8dab1586292b5fda3a063095
Mariko Sudo <sudo@fairground.jp>
2021-10-20 21:03:45 +0900

    meta viewportを書き換える

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import '../styles/globals.scss'
 const MyApp = ({ Component, pageProps }) => (
   <>
     <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <script defer src="//ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
       <script defer src="/js/script.js"></script>
     </Head>
